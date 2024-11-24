4ecd2575a4abc6fb4a4395db0b0f11a61ea33896
Mark19971014 <s194751@student.dtu.dk>
2024-11-24 10:25:45 +0100

    main

--- hello.py
+++ hello.py
@@ -16,10 +16,25 @@ def hello2(name)
 
 def hello3(first, last)
 
+def add(x,y)
+    return y + x 
 
+def add(x,y)
+    return x + y 
+
+    
 def hello(to="world"):
     print("hello,", to)
 
 main()
+#junhao
+
+# I am xiaohuan
 
 # I am test git push
+#版本控制
+# version control 
+
+
+def addnew():
+    print("hello git")
