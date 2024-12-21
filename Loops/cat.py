3f0056d2da1b7ed93e8f1f27e56372f3016170e7
Mark19971014 <s194751@student.dtu.dk>
2024-12-21 23:55:06 +0100

    Update Exception

---
+++ Loops/cat.py
@@ -0,0 +1,16 @@
+studentsMap = {
+    #Key-Value
+    "Hermoine": "Gryffindor",
+    "Harry": "Gryffindor",
+    "Ron": "Gryffindor",
+    "Draco": "Slytherin",
+}
+mylist = [1,2,5]
+for element in mylist:
+    print(elements)
+
+
+for student in studentsMap:
+    print(students["Harry"])
+    '
+#print("meow\n" * 3, end="")
