19743390c409e70a40216e2d7f8947a2788e30e5
ZzZZzzZZ-0023 <1462825044@qq.com>
2025-01-05 17:45:41 +0800

    hw

---
+++ xiaohuanHW/generator.py
@@ -0,0 +1,19 @@
+import random
+import statistics
+import numpy
+
+# coin = random.choice(["head","talis"])
+# print(coin)
+
+cards=["queen","ace","jack"]
+random.shuffle(cards)
+# print(cards[2])
+
+# for i in range(3):
+#     print(cards[i])
+
+#for each loop
+# for element in cards:
+#     print(element)
+
+print(statistics.mean([10,10,10]))
