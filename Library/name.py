673717a368e475ceadaabbdefd26d5c967634452
Mark19971014 <s194751@student.dtu.dk>
2025-01-25 23:09:22 +0100

    add oop

--- Library/name.py
+++ Library/name.py
@@ -1,4 +1,20 @@
-import sys 
+#import sys 
 
-print("hello", sys.argv[1])
+#print("hello", sys.argv[1])
 
+
+
+#argv = [1,2,3]
+#for element in argv:
+#    print("hello" + element)
+
+#xiaohuan:
+#hello 1
+#hello 2 
+#hello 3 
+
+
+
+
+
+ 
