8da6d35ddec7fe7e141c8b53d74f555fcdeb99f5
ZzZZzzZZ-0023 <1462825044@qq.com>
2025-03-02 10:32:49 +0100

    xiaohuan oop homework

--- mathHW/mathHW.py
+++ mathHW/mathHW.py
@@ -49,3 +49,45 @@ import numpy as np
 # import math
 # t=math.log(20)/0.195
 # print(t)
+
+# import math
+# T=10000*(1+0.055/4)**(11/3)*4
+# print(T)
+
+# import math
+# T=10000*(1+0.055/12)**(11/3)*12
+# print(T)
+
+# import math
+# Q=10000*math.exp(0.055*(3/11))
+# print(Q)
+
+# import math
+# t=math.log(2,1.03)/4
+# print(t)
+
+# import math
+# t=math.log(2,1.01)/12
+# print(t)
+
+# import math
+# t=math.log(2)/0.12
+# print(t)
+
+# import math
+# t=math.log(6.4)/5
+# print(t)
+
+# import math
+# t=math.log(8)/0.37
+# print(t)
+
+x=np.linspace(0.000001,10000.1, 100)
+f1=np.log10(x)
+g=np.log(x)
+plt.figure()
+plt.plot(x,f1,"r",x,g,"g")
+plt.show()
+
+
+
--- xiaohuanHW/oopHW.py
+++ xiaohuanHW/oopHW.py
@@ -1,17 +1,63 @@
-class Student:
-    def _init_(self,name,gender,age):
-        self.name = name
-        self.gender = gender
-        self.age =age
-
-def main():
-    student1 = ("xiaohuan", "female","19")
-
-def get_student():
-    name = input("Name: ")
-    gender = input("gender: ")
-    age = input("age:")
-    return Student(name, gender,age)
-
-
-    
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [
+    {
+     "ename": "SyntaxError",
+     "evalue": "invalid syntax (746885781.py, line 17)",
+     "output_type": "error",
+     "traceback": [
+      "\u001b[1;36m  Cell \u001b[1;32mIn[15], line 17\u001b[1;36m\u001b[0m\n\u001b[1;33m    class room1= Residence(self)\u001b[0m\n\u001b[1;37m               ^\u001b[0m\n\u001b[1;31mSyntaxError\u001b[0m\u001b[1;31m:\u001b[0m invalid syntax\n"
+     ]
+    }
+   ],
+   "source": [
+    "class Residence:\n",
+    "    def __init__(self, number: int, size: double, type: str):\n",
+    "        self.number = number\n",
+    "        self.size = size\n",
+    "        self.type = type\n",
+    "           \n",
+    "    def get_number(self):\n",
+    "        return self.number\n",
+    "\n",
+    "    def get_size(self):\n",
+    "        return self.size\n",
+    "        \n",
+    "    def get_type(self):\n",
+    "        return self.type\n",
+    "\n",
+    " \n",
+    "class Room(self):\n",
+    " def __init__(self, number: int, size: double):\n",
+    "     \n",
+    "class Aprttment(self):\n",
+    " def __init__(self, number: int, size: double , numberofroms: int):\n"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.6"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
